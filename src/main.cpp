a13041d7c235c2b188bff48eae8da4dc47364e46
kunal-new <nishantdeep121@gmail.com>
2025-12-24 02:42:13 +0530

    main update

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,37 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include "ast.h"
 
 extern int yyparse();
+extern FILE* yyin;
+extern ASTNode* root;
 
-int main() {
-    std::cout << "Enter an expression:\n";
-    yyparse();
+int main(int argc, char** argv) {
+    if (argc > 1) {
+        FILE* file = fopen(argv[1], "r");
+        if (!file) {
+            fprintf(stderr, "Error: Cannot open file '%s'\n", argv[1]);
+            return 1;
+        }
+        yyin = file;
+    }
+    
+    int parseResult = yyparse();
+    
+    if (argc > 1) {
+        fclose(yyin);
+    }
+    
+    if (parseResult != 0) {
+        fprintf(stderr, "Parsing failed\n");
+        return 1;
+    }
+    
+    if (root) {
+        RuntimeContext ctx;
+        root->execute(ctx);
+        delete root;
+    }
+    
     return 0;
 }
